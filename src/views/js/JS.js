df6d11065c432e66bc57af0f83541ef074cbc0bc
llovo-code <31813226+llovo-code@users.noreply.github.com>
2021-01-02 23:03:32 -0600

    First Commit developer web page uploaded all files

---
+++ src/views/js/JS.js
@@ -0,0 +1,39 @@
+
+AOS.init({
+    duration: 800,
+    ease: 'ease-in-out-sine'
+});
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 900,
+    speedAsDuration: true
+});
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        500:{
+            items:2
+        },
+        1000:{
+            items:3
+        },
+    }
+});
+
+/*$(document).ready(function() { 
+    $('.itemNav').click(function(){
+        var $target = $('.navbar-collapse');
+        if($target.hasClass('in')){
+            $target.removeClass('in').height(0).css('overflow','hidden');                           
+        }
+    });			
+});
+*/
