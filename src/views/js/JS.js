d27b00b08574839c2fe734285975f3f8f8a68465
llovo-code <31813226+llovo-code@users.noreply.github.com>
2021-01-23 16:06:39 -0600

    eliminar comentarios en features

--- src/views/js/JS.js
+++ src/views/js/JS.js
@@ -1,4 +1,3 @@
-
 AOS.init({
     duration: 800,
     ease: 'ease-in-out-sine'
@@ -10,32 +9,21 @@ var scroll = new SmoothScroll('a[href*="#"]', {
 });
 
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
+    loop: true,
+    margin: 10,
     autoplay: true,
     autoplayTimeout: 4000,
     dots: false,
-    responsive:{
-        0:{
-            items:1
+    responsive: {
+        0: {
+            items: 1
         },
-        500:{
-            items:2
+        500: {
+            items: 2
         },
-        1000:{
-            items:3
+        1000: {
+            items: 3
         },
     }
 });
 /*JS DE TIMELINE*/
-
-
-/*$(document).ready(function() { 
-    $('.itemNav').click(function(){
-        var $target = $('.navbar-collapse');
-        if($target.hasClass('in')){
-            $target.removeClass('in').height(0).css('overflow','hidden');                           
-        }
-    });			
-});
-*/
