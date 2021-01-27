8a92f137992747a7291dbb27cf7b38602f2398ae
WillFR98 <wledgardo55@gmail.com>
2021-01-27 16:19:30 -0600

    login

--- src/public/js/JS.js
+++ src/public/js/JS.js
@@ -141,3 +141,31 @@ function mouseovercards() {
         $(this).find('p').css('display', 'block');
     });
 }
+
+
+$(function() {
+
+    $(".progress").each(function() {
+
+        var value = $(this).attr('data-value');
+        var left = $(this).find('.progress-left .progress-bar');
+        var right = $(this).find('.progress-right .progress-bar');
+
+        if (value > 0) {
+            if (value <= 50) {
+                right.css('transform', 'rotate(' + percentageToDegrees(value) + 'deg)')
+            } else {
+                right.css('transform', 'rotate(180deg)')
+                left.css('transform', 'rotate(' + percentageToDegrees(value - 50) + 'deg)')
+            }
+        }
+
+    })
+
+    function percentageToDegrees(percentage) {
+
+        return percentage / 100 * 360
+
+    }
+
+});
